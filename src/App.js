79e3b34879d6e234665ce45b6872c13af5f16bc9
Riaz Islam <riazislam0999@gmail.com>
2021-01-05 23:58:34 +0600

    from component add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Form from './component/student_form/student_form'
 function App() {
   return (
     <div className="App">
-     
+     <Form />
     </div>
   );
 }
---
+++ src/component/student_form/student_form.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class From extends React.Component {
+   constructor(props) {
+      super(props);
+      
+      this.state = {
+         data: 'Initial data...'
+      }
+      this.updateState = this.updateState.bind(this);
+   };
+   updateState(e) {
+      this.setState({data: e.target.value});
+   }
+   render() {
+      return (
+         <div>
+         <div  style={{height:"600px",border: "1px solid red",margin:"5% 10% 5%",marginRight:"10%" }}>
+         <h1 >React Form </h1>
+            <input type = "text" value = {this.state.data} 
+               onChange = {this.updateState} style={{color: "red",width:"200px"}} />
+            <h4>{this.state.data}</h4>
+         </div>
+            
+         </div>
+      );
+   }
+}
+export default From;
